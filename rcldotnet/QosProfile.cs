03c2ee24583c33cda43319e89a3cc527537634eb
Camelron <cam.thebaird@gmail.com>
2020-07-02 11:35:47 -0700

    Plumb QoS profiles through to C# layer

---
+++ rcldotnet/QosProfile.cs
@@ -0,0 +1,17 @@
+
+
+namespace ROS22
+{
+    public static class QosProfile
+    {
+        public static enum Profile 
+        {
+            SENSOR_DATA = 0,
+            PROFILE_PARAMETERS = 1,
+            DEFAULT = 2,
+            SERVICES_DEFAULT = 3,
+            PARAMETER_EVENTS = 4,
+            SYSTEM_DEFAULTS = 5,
+        }
+    }
+}
